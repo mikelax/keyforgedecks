e45fea1ede078c50935fe0ce3899f80afdb5e3e6
Michael Holtzman <mholtzman@gmail.com>
2019-12-30 10:41:59 -0500

    chore: update date.

--- site/src/pages/index.js
+++ site/src/pages/index.js
@@ -34,7 +34,7 @@ class Index extends React.Component {
 export const query = graphql`
          query {
            keyforgedecks {
-             listRegisteredDecksCounts(filter: { date: "2019-12-29" }) {
+             listRegisteredDecksCounts(filter: { date: "2019-12-28" }) {
                timestamp
                count
              }
